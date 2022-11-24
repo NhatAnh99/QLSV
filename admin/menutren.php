947406139977d87fd51b4c9c92c8dd91ebadf63c
NhatAnh99 <nhatanhle6343322@gmai.com>
2022-11-24 15:44:04 +0700

    cap nhat lai menu tren

--- admin/menutren.php
+++ admin/menutren.php
@@ -1,25 +1,18 @@
 <?php
 	include('connect.php');
 ?>
-
-<!-- <h2>Welcome John</h2> -->
-<!-- <p id="page-intro">What would you like to do?</p> -->
 <ul class="shortcut-buttons-set">
-	<li><a class="shortcut-button" href="index.php?quanly=thongbao&id_gv=<?php echo $_SESSION['id_gv']  ?>">
-		<span><img src="resources/images/icons/paper_content_pencil_48.png" alt="icon" /><br />
-				Tạo thông báo
-		</span></a></li>
-	<li><a class="shortcut-button" href="index.php?quanly=chuyennhom&id_gv=<?php echo $_SESSION['id_gv']  ?>">
-		<span><img src="resources/images/icons/image_add_48.png" alt="icon" /><br />
-					Chuyển nhóm
-		</span></a></li> 
-	<li><a class="shortcut-button" href="index.php?quanly=yeucau&id_gv=<?php echo $_SESSION['id_gv']  ?>">
-		<span><img src="resources/images/icons/clock_48.png" alt="icon" /><br />
-					Yêu cầu
-		</span></a></li>
-				
-	<!-- <li><a class="shortcut-button" href="#messages" rel="modal">
-		<span><img src="resources/images/icons/comment_48.png" alt="icon" /><br />
-					Tin nhắn
-		</span></a></li>	 -->		
+    <li><a class="shortcut-button" href="index.php?quanly=thongbao&id_gv=<?php echo $_SESSION['id_gv']  ?>">
+            <span><img src="resources/images/icons/paper_content_pencil_48.png" alt="icon" /><br />
+                Tạo thông báo
+            </span></a></li>
+    <li><a class="shortcut-button" href="index.php?quanly=chuyennhom&id_gv=<?php echo $_SESSION['id_gv']  ?>">
+            <span><img src="resources/images/icons/image_add_48.png" alt="icon" /><br />
+                Chuyển nhóm
+            </span></a></li>
+    <li><a class="shortcut-button" href="index.php?quanly=yeucau&id_gv=<?php echo $_SESSION['id_gv']  ?>">
+            <span><img src="resources/images/icons/clock_48.png" alt="icon" /><br />
+                Yêu cầu
+            </span></a></li>
+
 </ul>
